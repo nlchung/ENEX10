addf3c62f72485c785e93032f574d20f56087d51
nlchung <45378799+nlchung@users.noreply.github.com>
2019-05-15 19:32:22 -0700

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,20 @@
+/*global $*/
+
+$(document).ready(function(){
+    $('.modal1').modal('attach events', '.open-modal-1', 'toggle');
+        
+    $('.modal2').modal('attach events', '.open-modal-2', 'toggle');
+        
+    $('.modal3').modal('attach events', '.open-modal-3', 'toggle');
+        
+    $('.modal4').modal('attach events', '.open-modal-4', 'toggle');
+    
+    $('.modal5').modal('attach events', '.open-modal-5', 'toggle');
+    
+    $('.modal6').modal('attach events', '.open-modal-6', 'toggle');
+    
+    $('.modal7').modal('attach events', '.open-modal-7', 'toggle');
+    
+    $('.modal8').modal('attach events', '.open-modal-8', 'toggle');
+    
+})
---
+++ slider.js
@@ -0,0 +1,11 @@
+/*global $*/
+
+$(document).ready(function(){
+  $('.slider-photos').slick({
+    accessibility: true,
+    fade: true,
+    arrows: false,
+    autoplay: false
+  });
+});
+
